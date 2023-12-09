335444fc47fe594809b7a015af2dbb1f61baaed5
Microcorporate Technologies (Pvt) Ltd <108064893+microcorporate@users.noreply.github.com>
2023-12-09 18:32:11 +0500

    journal entry model

--- account_profile_model.dart
+++ account_profile_model.dart
@@ -1,67 +1,62 @@
-import 'package:ouditor/models/attach_file_model/attach_file_model.dart';
-import 'package:ouditor/modules/accounting/models/account_status_enum/account_status_enum.dart';
-import 'package:ouditor/modules/accounting/models/account_subtypes_enum/account_subtypes_enum.dart';
-import 'package:ouditor/modules/accounting/models/account_types_enum/account_types_enum.dart';
+import 'package:ouditor/modules/accounting/models/journal_entry_types_enum/journal_entry_types_enum.dart';
+import 'package:ouditor/modules/accounting/models/ledger_entry_model/ledger_entry_model.dart';
 import 'package:ouditor/modules/audit_trail/models/audti_log/audit_log_model.dart';
 
-class AccountProfileModel {
+class JournalEntryModel {
   // firestore
   String documentId;
   DateTime docCreationDate;
 
-  // data structure
-  String parentAccount;
-  List<String> subAccountsList;
-
-  // audit log
+  // audit log 
   AuditLogModel auditLog;
-  List<AccountProfileModel> auditHistory;
+  List<JournalEntryModel> auditHistory;
 
   // data profile
-  String accountName;
-  String accountDescription;
-  String additionalInfo;
-  int accountNumber;
+  int journalEntryNumber;
+  DateTime journalEntryDate;
+  double journalEntryTotal;
+  JournalEntryTypesEnum journalEntryType;
+  List<LedgerEntryModel> ledgerEntries;
 
-  AccountTypesEnum accountType;
-  AccountSubTypesEnum accountSubType;
-  AccountStatusEnum accountStatus;
+  String attachFiles;
+  String memo;
 
-  DateTime openingDate;
-  double currentBalance;
+  JournalEntryModel(
+      {
+      required this.documentId,
+      required this.docCreationDate,
+      required this.auditLog,
+      required this.auditHistory,
+      required this.journalEntryDate,
+      required this.journalEntryNumber,
+      required this.journalEntryType,
+      required this.journalEntryTotal,
+      required this.ledgerEntries,
+      required this.attachFiles,
+      required this.memo,
+      });
+}
 
-  String accountMemo;
-  List<AttachFileModel> attachFiles;
 
-  AccountProfileModel({
-    required this.documentId,
-    required this.docCreationDate,
-    required this.parentAccount,
-    required this.subAccountsList,
-    required this.accountType,
-    required this.accountMemo,
-    required this.accountSubType,
-    required this.accountNumber,
-    required this.accountName,
-    required this.accountDescription,
-    required this.additionalInfo,
-    required this.openingDate,
-    required this.accountStatus,
-    required this.currentBalance,
-    required this.auditLog,
-    required this.auditHistory,
-    required this.attachFiles,
+
+class LedgerEntryModel {
+  String accountId;
+  String description;
+  double amount;
+  LedgerEntryTypesEnum ledgerEntryType;
+  String userUID;
+  bool billable;
+  String classification;
+  String location;
+
+  LedgerEntryModel({
+    required this.accountId,
+    required this.amount,
+    required this.billable,
+    required this.classification,
+    required this.description,
+    required this.ledgerEntryType,
+    required this.location,
+    required this.userUID,
   });
- void accountBalanceUpdate(){}
- void accountStatusUpdate(){}
- // audit log
- void createAuditLog(){}
- void restoreFromAuditLog(){}
- void deleteAuditLog(){}
-// Editingcontroller
- void fromEditingcontroller(){}
- void modifyWithEditingController(){}
- // serialization
- void fromJson(){}
- void toJson(){}
 }
